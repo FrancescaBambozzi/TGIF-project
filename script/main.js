6338b8ac8b8ca26ae063a0b39b174006aa3e2d65
Francesca <bambozzif@gmail.com>
2019-02-05 16:22:42 +0100

    senate-table-finish

--- script/main.js
+++ script/main.js
@@ -4,16 +4,16 @@ var members = data.results[0].members;
 
 // Step 2. Create a loop over that array.
 
-   /* create a new array of newMembArrayects (newMembArray === each member profile) containing only the properties needed.
+   /* create a new array (newMembArray === each member profile) containing only the properties needed.
       - the .map() method iterate through each element of the array
       - then, it creates a function that perform like this: 
         inside the new array(newMembArray) STORE only the elements with the following properties: */
 
 function createNewArray(){
 
-window.newMembArray = members.map(person => ({ Name: person.first_name, //use window.var object to make the var global
-                                               MiddleName:person.middle_name, 
-                                               Surname: person.last_name, 
+//use window.var object to make the var global
+window.newMembArray = members.map(person => ({ Name: [person.first_name, person.middle_name, person.last_name].join(" "),
+                                               //to join all these values inside one property.
                                                Party: person.party, 
                                                State: person.state, 
                                                Seniority: person.seniority, 
@@ -27,34 +27,50 @@ window.newMembArray = members.map(person => ({ Name: person.first_name, //use wi
 function generateTable(){
     
     var table = document.getElementById("senate-data"); //give this ID to the table in HTM
-    var header = table.createTHead();
-    var row = header.insertRow(0);
-    var cell = row.insertCell(0);
-    cell.innerHTML = "<b>This is a table header</b>";
-
-    /*
+    
+    var header = table.createTHead();   
+    var rowHead = header.insertRow(0);
+    
+    var name = rowHead.insertCell(0);
+    name.innerHTML = "Name";
+    var name = rowHead.insertCell(1);
+    name.innerHTML = "Party";
+    var name = rowHead.insertCell(2);
+    name.innerHTML = "State";
+    var name = rowHead.insertCell(3);
+    name.innerHTML = "Seniority";
+    var name = rowHead.insertCell(4);
+    name.innerHTML = "Votes";
+    
+    var body = document.createElement("tbody");
+  
 for (var i = 0; i < newMembArray.length; i++) {
     
-    var row = table.insertRow(i);
-   
+    var row = body.insertRow(i);
+    
     for(key in newMembArray[i]){
-
-      var cell = row.insertCell(0);
+        
+      var cell = row.insertCell();
       var cellContent = newMembArray[i][key];
         
         if(key === 'Name'){
             
             var cellLink = members[i].url;
             cell.innerHTML = cellContent.link(cellLink);
-
                 
-        } else {
+        } else if(key === 'Votes'){
+            
+            cell.innerHTML = cellContent +  ' %'; 
+        }
+        
+        else {
             
             cell.innerHTML = cellContent;
         }
-    } */
+    } 
 
 };
+      table.append(body); //the append() method inserts specified content(body) at the end of the selected elements(table)
 
 };
 
